aaefd70a7a3f5ae17d36f9efa5aebc42cf56d2f4
ZhuJunray <96456375+ZhuJunray@users.noreply.github.com>
2023-12-16 23:19:27 +0800

    rf_class

--- src/ml4content_auth.py
+++ src/ml4content_auth.py
@@ -487,19 +487,10 @@ def knn4con_binary_kfolds(authentications_per_person, user_names, dates, n_neigh
     average_accuracy = np.mean(accuracies)
     print("Average Accuracy:", average_accuracy, "\nprecision:", precisions, "\nrecalls:", recalls, "\nf1s:", f1s)
 
-def rf4con_binary_kfolds(authentications_per_person, user_names, dates, n_estimators=100, n_splits=5):
-    """
-    使用随机森林算法进行K折交叉验证。
-    
-    参数:
-    authentications_per_person -- 每个人的验证次数
-    user_names -- 用户名列表
-    dates -- 日期列表
-    n_estimators -- 随机森林中的树的数量
-    n_splits -- K折交叉验证的折数
-    """
+def rf_classification(authentications_per_person, user_names, dates, n_estimators=100):
     # 生成示例数据
-    labels = np.array([0 if user == 'zs' else 1 for user in user_names for _ in range(authentications_per_person)])
+    num_people = len(user_names)
+    labels = np.repeat(np.arange(num_people), authentications_per_person)
     data = data_processing(authentications_per_person=authentications_per_person, user_names=user_names, dates=dates,
                            rotdir=os.path.join(os.getcwd(), 'data'))
 
@@ -507,18 +498,30 @@ def rf4con_binary_kfolds(authentications_per_person, user_names, dates, n_estima
     scaler = StandardScaler()
     data_scaled = scaler.fit_transform(data)
 
+    # 划分数据集
+    X_train, X_test, y_train, y_test = train_test_split(data_scaled, labels, test_size=0.2)
+
     # 创建随机森林模型
     rf_model = RandomForestClassifier(n_estimators=n_estimators)
+    rf_model.fit(X_train, y_train)
+    y_pred = rf_model.predict(X_test)
 
-    # 创建K折交叉验证器
-    kf = KFold(n_splits=n_splits)
+    # 评估模型性能
+    accuracy = accuracy_score(y_test, y_pred)
+    print("准确度:", accuracy)
 
-    # 执行K折交叉验证
-    cv_scores = cross_val_score(rf_model, data_scaled, labels, cv=kf)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print("混淆矩阵:")
+    print(conf_matrix)
 
-    # 打印交叉验证结果
-    print(f'交叉验证得分: {cv_scores}')
-    print(f'平均得分: {cv_scores.mean()}')
+    precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='weighted')
+    print("精确度:", precision)
+    print("召回率:", recall)
+    print("F1分数:", f1)
+
+    class_report = classification_report(y_test, y_pred)
+    print("分类报告:")
+    print(class_report)
 
 ################################################################ main
 def main():
@@ -553,8 +556,8 @@ def main():
     print("-----------svm_multi_kfold------------")
     svm4con_multi_kfolds(authentications_per_person=authentications_per_person, user_names=user_names, dates=dates)
 
-    # print("---------rf_kfold------------")
-    # rf4con_binary_kfolds(authentications_per_person=authentications_per_person, user_names=user_names, dates=dates)
+    print("---------rf_classification------------")
+    rf_classification(authentications_per_person=authentications_per_person, user_names=user_names, dates=dates)
 
 if __name__ == "__main__":
     main()
